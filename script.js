1bde9086ee2f7d82fe858d2e9f51717a95113e8a
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-03-20 20:33:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+let input1 = document.getElementById("name");
+let input2 = document.getElementById("year");
+let button = document.getElementById("button");
+let h3 = document.getElementById("url");
+let form = document.getElementById("form");
+form.addEventListener('submit',(event)=>{
+	event.preventDefault();
+	let baseUrl = "https://localhost:8080/";
+    let params = [];
+
+    if (input1.value) params.push(`name=${encodeURIComponent(input1.value)}`);
+    if (input2.value) params.push(`year=${encodeURIComponent(input2.value)}`);
+
+    if (params.length > 0) {
+        h3.innerText = baseUrl + "?" + params.join("&");
+    }
+})
